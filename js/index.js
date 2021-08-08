c7a7a3c1fb88eb45e7956df2baa4383661b53ed6
Krishna <jh.krishna.chang@gmail.com>
2021-08-08 16:39:14 +0900

    add variable for logo container, added click event listener

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
+const logo_container = document.querySelector('.logo-container')
+
 const logo_animation = bodymovin.loadAnimation({
-    container: document.querySelector('.logo-container'), // Required
-    path: 'file:///Users/junhyungchang/Documents/startpage4/src/jun_filled_stroke.json', // Required
-    renderer: 'svg', // Required
-    loop: false, // Optional
-    autoplay: true, // Optional
-    name: 'logo', // Name for future reference. Optional.
+    container: logo_container,
+    path: 'https://raw.githubusercontent.com/MigguLarge/startpage4/master/src/jun_filled_stroke.json',
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    name: 'logo',
 });
 
 logo_animation.setSpeed(2);
+
+logo_container.addEventListener('click', (e) => {
+    logo_animation.goToAndPlay(0, true)
+})
